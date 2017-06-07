6aea97fb6800253c4c67bd6a78b02c74d76dcc36
石俊涛 <1550637676@qq.com>
2017-06-07 23:42:45 +0800

    位运算

--- 位运算/位运算/test.cpp
+++ 位运算/位运算/test.cpp
@@ -1,5 +1,10 @@
+/*
+本项目；主要用来记录比较经典得位运算解题方法
+*/
+
 #include <iostream>
 #include "判断一个数是否是2的乘方.h"
+
 using namespace std;
 
 int main()
--- 位运算/位运算/判断一个数是否是2的乘方.h
+++ 位运算/位运算/判断一个数是否是2的乘方.h
@@ -5,3 +5,18 @@ bool isPowerOf2(int number)
 {
 	return 0 == (number & number - 1); //优先级顺序：-、&（逻辑与）、==
 }
+
+//判断一个数的二进制中1的个数
+int getCountOf1(int number)
+{
+	int count = 0;
+	
+	whlie (number)
+	{
+		++count;
+		
+		number &= number-1;
+	}
+	
+	return count;
+}
